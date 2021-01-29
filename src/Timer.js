22bcd687c74f3052e371df11e11118ecd9996d81
lenmorld2 <35879780+lenmorld2@users.noreply.github.com>
2021-01-29 07:37:15 -0500

    Stop timer on button click

--- src/Timer.js
+++ src/Timer.js
@@ -7,10 +7,12 @@ class Timer extends React.Component {
     this.state = {
       value: Number(this.props.initial)
     };
+
+    this.intervalRef = React.createRef();
   }
 
   componentDidMount() {
-    setInterval(() => {
+    this.intervalRef.current = setInterval(() => {
       if (this.state.value > 0) {
         this.setState((prevState) => ({
           value: prevState.value - 1
@@ -19,8 +21,17 @@ class Timer extends React.Component {
     }, 1000);
   }
 
+  stopTimer = () => {
+    clearInterval(this.intervalRef.current);
+  };
+
   render() {
-    return <div>{this.state.value}</div>;
+    return (
+      <div>
+        {this.state.value}
+        <button onClick={this.stopTimer}>STOP</button>
+      </div>
+    );
   }
 }
 
